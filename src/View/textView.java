5ecffe39a070c31da69065e8c991d3cc7d4ed45e
RômuloManciola <RmuloManciola@192.168.25.5>
2015-10-31 14:59:17 -0200

    Simulação do jogo em modo texto

--- src/View/textView.java
+++ src/View/textView.java
@@ -25,17 +25,17 @@ public class textView {
         jogoController = new JogoController();
 
         System.out.println("Batalha Terrestre");
-        System.out.println("Colhendo credenciais...");
+        System.out.print("Colhendo credenciais...");
         Jogador jogador1 = new Jogador(JOptionPane.showInputDialog("Seu nome"));
-        System.out.print("Confirmadas");
-        System.out.println("Rastreando Inimigo...");
+        System.out.println("Confirmadas");
+        System.out.print("Rastreando Inimigo...");
         Jogador jogador2 = new Jogador(JOptionPane.showInputDialog("Inimigo"));
-        System.out.print("Encontrado");
+        System.out.println("Encontrado");
 
-        System.out.println("Iniciando solução diplomática...");
+        System.out.print("Iniciando solução diplomática...");
         Jogo jogo = new Jogo(10, jogador1, jogador2);
         jogoController.addJogo(jogo);
-        System.out.print("Diplomacia esgotada");
+        System.out.println("Diplomacia esgotada");
 
         System.out.println(jogador1.getNome() + ", escolha suas armas");
 
@@ -48,9 +48,9 @@ public class textView {
         jogo.getEstrategia1().addArma(new M60Patton());
         jogo.getEstrategia1().addArma(new L118());
 
-        System.out.println("Movendo arsenal...");
+        System.out.print("Movendo arsenal...");
         jogo.getEstrategia1().dispoeArmas();
-        System.out.print("Arsenal implantado");
+        System.out.println("Arsenal implantado");
 
         System.out.println(jogador1.getNome() + ", comande suas tropas");
 
@@ -69,9 +69,9 @@ public class textView {
         jogo.getEstrategia2().addArma(new M60Patton());
         jogo.getEstrategia2().addArma(new L118());
 
-        System.out.println("Movendo arsenal...");
+        System.out.print("Movendo arsenal...");
         jogo.getEstrategia2().dispoeArmas();
-        System.out.print("Arsenal implantado");
+        System.out.println("Arsenal implantado");
 
         System.out.println(jogador2.getNome() + ", comande suas tropas");
 
@@ -80,9 +80,16 @@ public class textView {
         imprimirGrelha(jogo.getEstrategia1().getGrelha());
 
         System.out.println("Confronto avistado");
+        System.out.print("Camuflando as tropas...");
+        System.out.println("Camufladas");
 
         System.out.println("Você detonou armas inimigas com sua M15");
         System.out.println("Você sofreu baixas");
+
+        imprimirJogo(jogo);
+
+        JOptionPane.showInputDialog("Qual coordenada, comandate " + jogador1.getNome() + "?");
+        JOptionPane.showInputDialog("Qual coordenada, comandate " + jogador2.getNome() + "?");
     }
 
     public static void imprimirGrelha(Grelha grelha) {
@@ -103,7 +110,51 @@ public class textView {
     }
 
     public static void imprimirJogo(Jogo jogo) {
+        Campo campo;
+        Campo[] campos;
+        Terra terra = new Terra();
+
         //List<Tiro> tiros;
+        System.out.println();
+
+        System.out.print("#");
+        for (int i = 0; i < jogo.getDimensao(); i++) {
+            System.out.print(" " + i);
+        }
+        System.out.print("\t");
+        System.out.print("#");
+        for (int i = 0; i < jogo.getDimensao(); i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < jogo.getDimensao(); i++) {
+            campos = jogo.getEstrategia1().getGrelha().getCampos()[i];
+            System.out.print(campos[0].getCoordenada().getX());
+            for (int j = 0; j < jogo.getDimensao(); j++) {
+                campo = campos[j];
+                System.out.print(" ");
+                if (campo.foiAtirado()) {
+                    System.out.print(campo.getObjeto().toString());
+                } else {
+                    System.out.print(terra.toString());
+                }
+            }
+            System.out.print("\t");
+            campos = jogo.getEstrategia2().getGrelha().getCampos()[i];
+            System.out.print(campos[0].getCoordenada().getX());
+            for (int j = 0; j < jogo.getDimensao(); j++) {
+                campo = campos[j];
+                System.out.print(" ");
+                if (campo.foiAtirado()) {
+                    System.out.print(campo.getObjeto().toString());
+                } else {
+                    System.out.print(terra.toString());
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 }
